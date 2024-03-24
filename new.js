bb7a635ccd84cb90cd9a82fb0823d505f42c1020
Raman Kumar Charak <withrkgaming78@gmail.com>
2024-03-24 17:18:38 +0530

    firstCommit

---
+++ new.js
@@ -0,0 +1,17 @@
+
+let a = new rsAnimate(document.querySelector("h2"));
+
+
+a.waveSplitInit({
+    repeat:false,
+    inAni: "flipY",
+    baseColor: "white",
+    inColor:"blue",
+    inDir:"left",
+    outDir:"right",
+    pauseTime:2000,
+    aniSpeed:5,
+    speedControl:false,
+    onView:false
+})
+
