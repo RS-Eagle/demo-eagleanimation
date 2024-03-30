5649c77d116c418d8ed6838c88bc3d4c703c7a8b
Raman Kumar Charak <withrkgaming78@gmail.com>
2024-03-30 23:21:08 +0530

    New Update

--- new.js
+++ new.js
@@ -1,17 +1,33 @@
+let animation = new rsAnimate(document.querySelector("h2"));
 
-let a = new rsAnimate(document.querySelector("h2"));
 
-
-a.waveSplitInit({
-    repeat:false,
-    inAni: "flipY",
-    baseColor: "white",
-    inColor:"blue",
-    inDir:"left",
-    outDir:"right",
-    pauseTime:2000,
-    aniSpeed:5,
-    speedControl:false,
-    onView:false
+animation.textAnimate({
+    path: document.querySelector(".ss"),
+    inAni: "wave",
+    onView:true,
+    pauseTime:1000,
+    aniSpeed:4.9
 })
 
+animation.textAnimate({
+        path: document.querySelector(".hi"),
+        repeat:true,
+        inAni: "topFade",
+        baseColor: "white",
+        inColor:"white",
+       
+})
+
+
+// ------All Valid Input----------
+//     path : document.querySelector(".hi") 
+//     repeat:true,
+//     inAni: "wave",
+//     baseColor: "white",
+//     inColor:"white",
+//     inDir:"left",
+//     outDir:"left",
+//     pauseTime:0,
+//     speedControl:false,
+//     aniSpeed:4.9,
+//     onView:false
